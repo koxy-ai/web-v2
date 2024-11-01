f217aaa83e8bd0a2e471ea388feb232351996174
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-11-01 14:19:48 +0000

    just logo update

---
+++ src/app/logo/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="flex items-center gap-0.5 min-w-max">
+        <div className="w-3 h-5 rounded-tl-[999px] border border-white/20  bg-white"></div>
+        <div className="w-3 h-5 rounded-br-[999px] border border-white/20  bg-white "></div>
+      </div>
+    </div>
+  );
+}
